a07cd51669da1af4b40282fe9dbd1dbf740f0c4e
Rodrigo Souza <rdrg.rcs1997@gmail.com>
2025-06-30 01:19:36 -0300

    mapProviderName

--- src/service/watchProviders.js
+++ src/service/watchProviders.js
@@ -8,10 +8,36 @@ const getWatchProviders = async (movieId, language = 'pt-BR') => {
     const { data } = await axios.get(url, { params });
     const providers = data.results && data.results.BR ? data.results.BR.flatrate || [] : [];
 
-    return providers.map(provider => ({
-      ...provider,
-      logo_path: provider.logo_path ? `https://image.tmdb.org/t/p/w92${provider.logo_path}` : null
-    }));
+    const seen = new Set();
+    const popularNames = {
+      'Amazon Prime Video': 'Prime Vídeo',
+      'Amazon Prime Video with Ads': 'Prime Vídeo',
+      'Netflix': 'Netflix',
+      'Netflix Standard with Ads': 'Netflix',
+      'Disney Plus': 'Disney+',
+      'HBO Max': 'Max',
+      'Max': 'Max',
+      'Globo': 'Globoplay',
+      'Globoplay': 'Globoplay',
+      'Claro': 'Claro video',
+      'Claro video': 'Claro video',
+      'Claro tv+': 'Claro tv+',
+      // Adicione outros nomes se necessário
+    };
+    const uniqueProviders = [];
+    for (const provider of providers) {
+      if (provider.provider_name.includes('Channel')) continue;
+      const popularName = popularNames[provider.provider_name] || provider.provider_name;
+      if (!seen.has(popularName)) {
+        seen.add(popularName);
+        uniqueProviders.push({
+          ...provider,
+          provider_name: popularName,
+          logo_path: provider.logo_path ? `https://image.tmdb.org/t/p/w92${provider.logo_path}` : null
+        });
+      }
+    }
+    return uniqueProviders;
   } catch (e) {
     return [];
   }
